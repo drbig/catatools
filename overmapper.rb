620e0a15e20fb542af40dce8d1c0cc5e65dba547
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-05-07 11:29:58 +0200

    Initial commit

---
+++ overmapper.rb
@@ -0,0 +1,224 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+#
+# See LICENSE.txt for licensing information.
+
+VERSION = '0.4'
+
+# Colors, RGBA hex
+CBG     = 0x000000ff # black
+CFG     = 0xffffffff # white
+CGRID   = 0xff0000ff # red
+CORIGIN = 0x00ff00ff # green
+
+@conf = {
+  mapx: 180, mapy: 180, level: 10, scale: 2, verbose: false,
+  grid: true, origin: true
+}
+
+## Guts
+
+# Handy struct for keeping general information about overmap
+# seen data
+Overview = Struct.new(:seen, :width, :height, :n, :s, :w, :e)
+class Overview
+  def any?; seen.keys.any?; end
+  def to_s
+    "Overview: #{width}x#{height} overmaps: #{seen.keys.length}"
+  end
+end
+
+# Print stuff only if verbose is on
+def verbose(&blk)
+  return unless @conf[:verbose]
+  puts blk.call
+end
+
+# Find seen files and record overview data
+def overview(save_path)
+  seen = Hash.new
+  n = w = s = e = 0
+
+  Dir.glob(File.join(save_path, '#*.seen.*')).each do |f|
+    x, y = f.match(/seen\.(-?\d+)\.(-?\d+)$/).captures.map(&:to_i)
+    seen["#{x} #{y}"] = f
+    w = x if x < w
+    e = x if x > e
+    n = y if y > n
+    s = y if y < s
+  end
+
+  width = e + w.abs + 1
+  height = n + s.abs + 1
+
+  Overview.new(seen, width, height, n, s, w, e)
+end
+
+# Extract chosen level raw data from a seen file and call the data parsing
+# function on it
+def parse_seen(path)
+  data = File.open(path) do |fh|
+    fh.each_line do |line|
+      next unless line.match(/L #{@conf[:level]}/)
+      break fh.readline.chop
+    end
+  end
+
+  parse_seen_data(data)
+end
+
+# Transform the raw seen data into an array of arrays of box coordinates for
+# drawing
+#
+# Box coordinates are overmap-local and unscaled. Does translate the RLE
+# into a set of 'scan lines', so to say...
+def parse_seen_data(data)
+  boxes = Array.new
+  position = 0
+
+  data.scan(/\d+ \d+/).map do |e|
+    visited, len = e.split.map(&:to_i)
+
+    if visited == 1
+      x0 = position % @conf[:mapx]
+      y0 = position / @conf[:mapx]
+      x1 = (position + len - 1) % @conf[:mapx]
+      y1 = (position + len - 1) / @conf[:mapx]
+
+      # split into 'scan lines' if needed
+      if y0 != y1
+        boxes.push([x0, y0, @conf[:mapx]-1, y0])
+        (y0+1).upto(y1).each do |sy|
+          if sy == y1
+            boxes.push([0, sy, x1, sy])
+          else
+            boxes.push([0, sy, @conf[:mapx]-1, sy])
+          end
+        end
+      else
+        boxes.push([x0, y0, x1, y1])
+      end
+    end
+
+    position += len
+  end
+
+  boxes
+end
+
+# Drawing loop helper
+#
+# As wee need to convert a number of coordinates it seems better to extract
+# the loop logic.
+#
+# x, y - overmap coordinates
+# ix, iy - image coordinates
+def draw_loop(overview, &blk)
+  iy = 0
+  overview.s.upto(overview.n).each do |y|
+    ix = 0
+    overview.w.upto(overview.e).each do |x|
+      blk.call(x, y, ix, iy)
+      ix += 1
+    end
+    iy += 1
+  end
+end
+
+# Transform box to image coordinates for given overmap
+def box_transform(box, ix, iy)
+  box[0] = (ix * XO) + (box[0] * @conf[:scale])
+  box[1] = (iy * YO) + (box[1] * @conf[:scale])
+  box[2] = (ix * XO) + ((box[2]+1) * @conf[:scale])
+  box[3] = (iy * YO) + ((box[3]+1) * @conf[:scale])
+
+  box
+end
+
+# Draw seen
+#
+# The main drawing loop
+def draw_seen(image, overview)
+  draw_loop(overview) do |x, y, ix, iy|
+    verbose { "At overmap #{x}x#{y}..." }
+    next unless overview.seen.has_key? "#{x} #{y}"
+    parse_seen(overview.seen["#{x} #{y}"]).each do |box|
+      image.rect(*box_transform(box, ix, iy), CFG, CFG)
+    end
+  end
+end
+
+# Draw grid
+def draw_grid(image, overview)
+  draw_loop(overview) do |x, y, ix, iy|
+    image.rect(ix*XO, iy*YO, (ix+1)*XO, (iy+1)*YO, CGRID)
+  end
+end
+
+# Draw origin
+def draw_origin(image, overview)
+  draw_loop(overview) do |x, y, ix, iy|
+    next unless x == 0 && y == 0
+    image.rect(ix*XO+1, iy*YO+1, (ix+1)*XO-1, (iy+1)*YO-1, CORIGIN)
+  end
+end
+
+## Main
+
+if __FILE__ == $0
+  require 'optparse'
+
+  op = OptionParser.new do |o|
+    o.banner = "Usage:\t#{$PROGRAM_NAME} [option...] save_path name.png\n\tVersion: #{VERSION}\n\n"
+    o.on('-h', '--help', 'Display this message') { puts o; exit }
+    o.on('-v', '--verbose', 'Enable debug output') { @conf[:verbose] = true }
+    o.on('-x', '--mapx MAPX', Integer, 'Set overmap width (MAPX)') {|a| @conf[:mapx] = a }
+    o.on('-y', '--mapy MAPY', Integer, 'Set overmap height (MAPY)') {|a| @conf[:mapy] = a }
+    o.on('-l', '--level L', Integer, 'Set map layer (L)') {|a| @conf[:level] = a }
+    o.on('-s', '--scale INT', Integer, 'Set pixel scale') {|a| @conf[:scale] = a }
+    o.on('-g', '--[no-]grid', 'Draw grid') {|a| @conf[:grid] = a }
+    o.on('-o', '--[no-]origin', 'Draw origin') {|a| @conf[:origin] = a}
+  end
+  op.parse!
+
+  unless ARGV.length == 2
+    puts op
+    exit(2)
+  end
+
+  PATH = ARGV.shift
+  OUTPUT = ARGV.shift
+
+  # x,y scale/offset consts
+  XO = @conf[:mapx] * @conf[:scale]
+  YO = @conf[:mapy] * @conf[:scale]
+
+  require 'chunky_png'
+
+  begin
+    puts 'Processing... (this may take some time)'
+
+    verbose { 'Gathering seen data...' }
+    overview = overview(PATH)
+    verbose { overview.to_s }
+
+    image = ChunkyPNG::Image.new(overview.width * @conf[:mapx] * @conf[:scale],
+                                 overview.height * @conf[:mapy] * @conf[:scale],
+                                 CBG)
+
+    verbose { 'Drawing seen data...' }
+    draw_seen(image, overview)
+
+    verbose { 'Drawing additional stuff...' }
+    draw_grid(image, overview) if @conf[:grid]
+    draw_origin(image, overview) if @conf[:origin]
+
+    verbose { 'Saving image...' }
+    image.save(OUTPUT)
+    puts 'Done.'
+  rescue StandardError => e
+    STDERR.puts "Error: #{e.to_s}"
+    STDERR.puts "Stack: #{e.backtrace.join("\n")}" if @conf[:verbose]
+    exit(3)
+  end
+end
---
+++ overmapper_test.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+#
+# See LICENSE.txt for licensing information.
+
+require 'minitest/autorun'
+require_relative 'overmapper.rb'
+
+class TestOvermapper < Minitest::Test
+  def setup
+    # default config
+    @conf = {
+      mapx: 180, mapy: 180, level: 10, scale: 2, verbose: false,
+      grid: true, origin: true
+    }
+  end
+
+  def test_parse_seen_data
+    [
+      ['1 10', [[0, 0, 9, 0]]],
+      ['0 10 1 5', [[10, 0, 14, 0]]],
+      ['1 200', [[0, 0, 179, 0], [0, 1, 19, 1]]],
+    ].each do |(input, output)|
+      assert_equal output, parse_seen_data(input)
+    end
+  end
+end
